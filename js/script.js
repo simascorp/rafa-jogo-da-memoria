59f8984f0da94744d1036430e474ab033382878a
felipeacelino <felipeacelino@hotmail.com>
2017-01-07 23:17:44 -0200

    Update

--- js/script.js
+++ js/script.js
@@ -261,8 +261,8 @@
 
 	}
 
-	var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
-	//var images = ['1.jpg'];
+	//var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
+	var images = ['1.jpg','2.jpg','3.jpg'];
 	
 	var game = new Game(images);
 	
